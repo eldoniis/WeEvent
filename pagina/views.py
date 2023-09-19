45ba9b19ea14f6769cb6493c6443a08aa11aa9db
bdcanop <bdcanop@eafit.edu.co>
2023-09-19 18:11:19 -0500

    crear evento y eliminarlo

--- pagina/views.py
+++ pagina/views.py
@@ -4,6 +4,7 @@ from django.views import View
 from django import forms
 from decimal import Decimal
 from .models import *
+from .forms import *
 
 class HomePage(TemplateView):
     template_name = 'home.html'
@@ -29,3 +30,23 @@ class EventShowView(View):
         viewData["event"] = get_object_or_404(Evento,pk=id)
         
         return render(request, self.template_name, viewData)
+    
+class CreateEventView(View):
+    template_name = 'create_event.html'
+
+    def get(self, request):
+        form = EventoForm()
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request):
+        form = EventoForm(request.POST)
+        if form.is_valid():
+            evento = form.save()
+            return redirect('show_event', id=evento.id)
+        return render(request, self.template_name, {'form': form})
+    
+class DeleteEventView(View):
+    def get(self, request, id):
+        evento = get_object_or_404(Evento, id=id)
+        evento.delete()
+        return redirect('events_index')
