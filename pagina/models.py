95f8b5c37ca40f7234d0056f1f38bb6a5bdde400
dagarcias <dagarcias@eafit.edu.co>
2023-09-19 10:50:52 -0500

    Create models

--- pagina/models.py
+++ pagina/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
+class Evento(models.Model):
+    class Tipo(models.TextChoices):
+        depo='deportes',_('deportes')
+        cult='cultural',_('cultural')
+        mus='musical',_('muscial')
+        gastr='gastronomico',_('gastronomico')
+    nombre=models.CharField(max_length=30)
+    capacidad=models.CharField(max_length=4)
+    tipoDePrenda=models.CharField(default='' , max_length=40)
+    tipo =models.CharField(max_length=13,choices=Tipo.choices,default=Tipo.depo)
+    descripcion=models.TextField
+    image=models.ImageField(upload_to='pagina/images', default='')
+    
 
+    
+    
