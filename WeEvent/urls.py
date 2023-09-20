d40444cef16ef72971cbe13b3dbad6b6fa3c62d5
sbetancurb <sbetancurb@eafit.edu.co>
2023-09-20 13:17:17 -0500

    Actualizaciones

--- WeEvent/urls.py
+++ WeEvent/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('pagina.urls')),
+    path('accounts/',include('accounts.urls')),
 ]
--- pagina/views.py
+++ pagina/views.py
@@ -1,52 +1,52 @@
 from django.shortcuts import render, get_object_or_404, redirect, reverse
 from django.views.generic import TemplateView
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
 from django.views import View
 from django import forms
 from decimal import Decimal
 from .models import *
 from .forms import *
 
-class HomePage(TemplateView):
+def HomePage(request):
     template_name = 'home.html'
 
-class EventIndexView(TemplateView):
+    return render(request, template_name)
+
+@login_required
+def EventIndexView(request):
     template_name = 'events_index.html'
 
-    def get(self, request):
-        viewData = {}
-        viewData["title"] = "Title of the view"
-        viewData["subtitle"] =  "Subtitle of the view"
-        viewData["events"] = Evento.objects.all()
+    viewData = {}
+    viewData["title"] = "Title of the view"
+    viewData["subtitle"] =  "Subtitle of the view"
+    viewData["events"] = Evento.objects.all()
 
-        return render(request, self.template_name, viewData)
+    return render(request, template_name, viewData)
     
-class EventShowView(View):
+def EventShowView(request,id,):
     template_name = 'show_event.html'
 
-    def get(self, request, id):
-        viewData = {}
-        viewData["title"] = "Title of the view"
-        viewData["subtitle"] =  "Subtitle of the view"
-        viewData["event"] = get_object_or_404(Evento,pk=id)
+    viewData = {}
+    viewData["title"] = "Title of the view"
+    viewData["subtitle"] =  "Subtitle of the view"
+    viewData["event"] = get_object_or_404(Evento,pk=id)
         
-        return render(request, self.template_name, viewData)
+    return render(request, template_name, viewData)
     
-class CreateEventView(View):
+def CreateEventView(request):
     template_name = 'create_event.html'
 
-    def get(self, request):
-        form = EventoForm()
-        return render(request, self.template_name, {'form': form})
-
-    def post(self, request):
-        form = EventoForm(request.POST)
-        if form.is_valid():
-            evento = form.save()
-            return redirect('show_event', id=evento.id)
-        return render(request, self.template_name, {'form': form})
+    form = EventoForm(request.POST)
+    print(form.is_valid())
+    if form.is_valid():
+        evento = form.save()
+        return redirect('show_event', id=evento.id)
+        
     
-class DeleteEventView(View):
-    def get(self, request, id):
-        evento = get_object_or_404(Evento, id=id)
-        evento.delete()
-        return redirect('events_index')
+    return render(request, template_name, {'form': form})
+
+def DeleteEventView(request,id):
+    evento = get_object_or_404(Evento, id=id)
+    evento.delete()
+    return redirect('events_index')
